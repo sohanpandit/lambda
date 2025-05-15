5c6d78a331ecc7d675945d179024797004acb497
sohan kumar <sohan.kumar@epsilon.com>
2025-05-15 20:12:17 +0530

    sdfd

--- src/lambda-profile/index.js
+++ src/lambda-profile/index.js
@@ -1,1 +1,1 @@
-//hello Mondaywwdff
+//hello Mondaywwdfffdhgd
--- src/user-info/index.mjs
+++ src/user-info/index.mjs
@@ -4,7 +4,7 @@ export const handler = async (event) => {
     const response = {
       statusCode: 200,
       body: {
-        personalizedMessage: `Hey ${user}, hope you are having a great day from github testsdfdfdsvdddfddfdd23`
+        personalizedMessage: `Hey ${user}, hope you are having a great day from github testsdfdfdsffvdddfddfdd23`
       },
     };
     return response;
