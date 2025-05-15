c3cb5c5c9906a3dcdcf3e9eda2bcc21e6f3cd08f
sohan kumar <sohan.kumar@epsilon.com>
2025-05-15 15:43:48 +0530

    t1

--- src/user-info/index.mjs
+++ src/user-info/index.mjs
@@ -4,7 +4,7 @@ export const handler = async (event) => {
     const response = {
       statusCode: 200,
       body: {
-        personalizedMessage: `Hey ${user}, hope you are having a great day from github test3`
+        personalizedMessage: `Hey ${user}, hope you are having a great day from github test13`
       },
     };
     return response;
